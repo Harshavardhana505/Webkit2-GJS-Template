f4ea14e8a26a2953aaa182addd9a33c5aa640506
Harshavardhana <75427218+Harshavardhana505@users.noreply.github.com>
2021-03-10 15:44:18 +0530

    Create Template.js

---
+++ Template.js
@@ -0,0 +1,107 @@
+#!/usr/bin/gjs
+
+/*
+GJS example showing how to build Gtk javascript applications
+using Webkit.WebView, also showing how to send messages from GTK
+to Webkit and vice versa
+
+Run it with:
+    gjs egWebmsg.js
+*/
+
+const Gio   = imports.gi.Gio;
+const GLib  = imports.gi.GLib;
+const Gtk   = imports.gi.Gtk;
+const Lang  = imports.lang;
+const Webkit = imports.gi.WebKit2;
+
+// Get application folder and add it into the imports path
+function getAppFileInfo() {
+    let stack = (new Error()).stack,
+        stackLine = stack.split('\n')[1],
+        coincidence, path, file;
+
+    if (!stackLine) throw new Error('Could not find current file (1)');
+
+    coincidence = new RegExp('@(.+):\\d+').exec(stackLine);
+    if (!coincidence) throw new Error('Could not find current file (2)');
+
+    path = coincidence[1];
+    file = Gio.File.new_for_path(path);
+    return [file.get_path(), file.get_parent().get_path(), file.get_basename()];
+}
+const path = getAppFileInfo()[1];
+imports.searchPath.push(path);
+
+const App = function () { 
+
+    this.title = 'Paint';
+    GLib.set_prgname(this.title);
+};
+
+App.prototype.run = function (ARGV) {
+
+    this.application = new Gtk.Application();
+    this.application.connect('activate', () => { this.onActivate(); });
+    this.application.connect('startup', () => { this.onStartup(); });
+    this.application.run([]);
+};
+
+App.prototype.onActivate = function () {
+
+    this.window.show_all();
+};
+
+App.prototype.onStartup = function() {
+
+    this.buildUI();
+};
+
+App.prototype.buildUI = function() {
+
+    this.window = new Gtk.ApplicationWindow({ application: this.application,
+                                              title: this.title,
+                                              default_height: 1000,
+                                              default_width: 1000,
+                                              window_position: Gtk.WindowPosition.CENTER });
+    try {
+        this.window.set_icon_from_file(path + '/assets/appIcon.png');
+    } catch (err) {
+        this.window.set_icon_name('application-x-executable');
+    }
+
+    this.window.add(this.getBody());
+};
+
+App.prototype.getBody = function() {
+
+    let webView, button, label, grid;
+
+    webView = new Webkit.WebView({ vexpand: true });
+    webView.load_uri(GLib.filename_to_uri (path + '', null));
+    webView.connect('notify::title', (self, params) => {
+        // Get Webkit messages into GTK listening to 'notify::title' signals
+        label.label = webView.title;
+    });
+
+    button = new Gtk.Button({ label: '' });
+    button.connect('clicked', () => {
+            // Execute one Webkit function to send a message from GTK to Webkit
+            webView.run_javascript('messageFromGTK("Message from GTK!");', null, (self, result, error) => {
+                self.run_javascript_finish(result);
+            });
+        });
+
+    label = new Gtk.Label({ label: '' });
+
+    grid = new Gtk.Grid({ column_homogeneous: true });
+    grid.attach (webView, 0, 0, 2, 1);
+    grid.attach (button, 0, 1, 1, 1);
+    grid.attach (label, 1, 1, 1, 1);
+
+    return grid;
+}; 
+
+//Run the application
+let app = new App();
+app.run(ARGV);
